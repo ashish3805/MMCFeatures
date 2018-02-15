df7bd659de7d94a36a7c6eba7d48224a5fd5c0cc
Ashish Kumar Patel <ashish_3805@hotmail.com>
2018-02-15 22:53:25 +0530

    started with new app

--- app.js
+++ app.js
@@ -1,60 +1,32 @@
-let APIClinet = require('./API');
-let client = new APIClinet('https://api.spotify.com');
+/**
+ * 1) Generate CSV.
+ * 2) Show Statistics about data.
+ * 2) Add More Data.
+ */
+
 let fs = require('fs');
-const readline = require('readline');
 
-let log = function (data) {
-  console.log(JSON.stringify(data));
-  fs.writeFileSync('./lastResponse.json', JSON.stringify(data));
-  console.log('\nSaved the output at:'+__dirname+'/lastResponse.json');
-};
-
-function getMe () {
-  let url = '/v1/users/jmperezperez';
-  client.requestGet(url).then(log, log);
-}
-
-function searchSong (song) {
-  let url = '/v1/search?q='+song+'&type=track';
-  client.requestGet(url).then(log, log);
-}
-
-// https://developer.spotify.com/web-api/console/tracks/
-// http://docs.echonest.com.s3-website-us-east-1.amazonaws.com/_static/AnalyzeDocumentation.pdf
-
-function getFeatures (trackId) {
-  let url = '/v1/audio-features/'+trackId;
-  client.requestGet(url).then(log, log);
-}
-
-function getAnalysis (trackId) {
-  let url = '/v1/audio-analysis/'+trackId;
-  client.requestGet(url).then(log, log);
-}
-
-let trackId = '4lUnFLtn8w3W76FiYDRzmO';
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Choose One using keyboard: \n1)Analyze Song \n2)Get Features \n3)exit\n', (answer) => {
-  switch (answer) {
-    case '1':
-      getAnalysis(trackId);
-      console.log(`Doing:  ${answer}`);
-      break;
-    case '2':
-      getFeatures(trackId);
-      console.log(`Doing:  ${answer}`);
-      break;
-    case '3':
-      process.exit(0);
-      break;
-    default:
-      console.log(`Wrong option:  ${answer}`);
-      break;
+class App {
+  constructor (configPath) {
+    this._config = JSON.parse(fs.readFileSync(configPath));
   }
-  rl.close();
-});
+
+  generateCSV () {
+    if (fs.existsSync(this._dataPath+'/lastFMResponses/meta.json')) {
+      // Do something
+    } else {
+
+    }
+  }
+
+  showStats () {
+
+  }
+
+  addMoreData () {
+
+  }
+}
+
+module.exports = App;
+
