8c923b172cf48d710ab526135bd310e4bd1bfe25
Ashish Kumar Patel <patelashish53@gmail.com>
2018-02-17 21:54:23 +0530

    started with utils

--- ashish/Spotify.js
+++ ashish/Spotify.js
@@ -73,19 +73,6 @@ class Spotify {
       }
     });
   }
-
-  static _wait (seconds) {
-    return new Promise((resolve, reject) => {
-      try {
-        if (seconds == 0) seconds = 1;
-        console.log('Will wait for ', seconds, ' seconds.');
-        setTimeout(resolve, seconds * 1000);
-      } catch (error) {
-        console.log(error);
-        reject(error);
-      }
-    });
-  }
 }
 
 module.exports = Spotify;
