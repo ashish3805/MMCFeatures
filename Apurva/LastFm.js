cc1abc09d1dd91e5e441b69b9c08da05c9cf7dcc
Apurva Maheshwari <apurvamaheshwari18@gmail.com>
2018-02-18 10:31:28 +0530

    Created LastFm.js class  and its methods

---
+++ Apurva/LastFm.js
@@ -0,0 +1,17 @@
+class LastFm {
+    constructor () {
+
+    }
+    ifAnyLeft (tag) {
+        console.log('Checking for more data');
+    }
+    getNextTrack (tag) {
+        console.log('getting new track');
+    }
+    fetchNextPage () {
+        console.log('fetching next page');
+    }
+    checkConsistency () {
+        console.log('checking consistency');
+    }
+}
